553d187de36edba0cef4627077df33e21ffee90a
Gia Lương <gialuong@MacBook-Pro-cua-Gia.local>
2022-08-26 09:39:36 +0700

    update

--- src/th_Student/Main.java
+++ src/th_Student/Main.java
@@ -5,17 +5,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-//    public static void writeToFile(String path, List<Student> students) {
-//        try {
-//            FileOutputStream fos = new FileOutputStream(path);
-//            ObjectOutputStream oos = new ObjectOutputStream(fos);
-//            oos.writeObject(students);
-//            oos.close();
-//            fos.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public static void writeToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static List<Student> readDataFromFile(String path){
         List<Student> students = new ArrayList<>();
@@ -32,13 +32,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-//        List<Student> students = new ArrayList<>();
-//        students.add(new Student(1, "Vũ Kiều Anh", "Hà Nội"));
-//        students.add(new Student(2, "Nguyễn Minh Quân", "Hà Nội"));
-//        students.add(new Student(3, "Đặng Huy Hoà", "Đà Nẵng"));
-//        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
-//        students.add(new Student(5, "Nguyễn Khắc Nhật", "Hà Nội"));
-//        writeToFile("student.txt", students);
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Vũ Kiều Anh", "Hà Nội"));
+        students.add(new Student(2, "Nguyễn Minh Quân", "Hà Nội"));
+        students.add(new Student(3, "Đặng Huy Hoà", "Đà Nẵng"));
+        students.add(new Student(4, "Nguyễn Khánh Tùng", "Hà Nội"));
+        students.add(new Student(5, "Nguyễn Khắc Nhật", "Hà Nội"));
+        writeToFile("student.txt", students);
 
 
         List<Student> studentDataFromFile = readDataFromFile("student.txt");
